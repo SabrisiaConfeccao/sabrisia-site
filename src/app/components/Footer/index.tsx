8281f9c4acc020fda87f8a95f8dfa661fca0c0b6
Jeazí Ricardo <jeazi_ricardo@hotmail.com>
2025-01-05 19:18:24 -0300

    feat: componente Footer

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="max-w-container mx-auto px-3 py-5 border-t-2 border-green-600">
+      <p className="font-semibold text-center">&copy; 2025 Sabrísia Confecção - Todos os direitos reservados</p>
+    </footer>
+  );
+}
