a9b051f2c07cb8fa578428332dfc751f974aed9c
Jeazí Ricardo <jeazi_ricardo@hotmail.com>
2025-01-19 19:30:21 -0300

    feat: finaliza a página sobre

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import { GridContainer } from '../components/GridContainer';
 import { Footer } from '../components/Footer';
@@ -7,26 +9,24 @@ function Sobre() {
   return (
     <>
       <Header />
-      <GridContainer className="h-[calc(100vh-10rem)] items-center mt-4">
-        <h1 className="text-4xl text-center font-bold mb-4">Sobre</h1>
-        <div className="flex items-center justify-around">
-          <div className="h-96 w-full flex justify-center mt-3">
-            <Image
-              src="/imagem-sobre.jpg"
-              alt="Sobre"
-              width={300}
-              height={300}
-              className="h-auto shadow-lg shadow-green-600 object-fill"
-            />
-          </div>
-          <section className="w-full">
-            <p className="text-2xl text-justify">
-              Somos uma empresa preocupada em oferecer produtos de alta qualidade,
-              aproveitando os recursos de maneira sustentável em busca de
-              satisfação do cliente.
-            </p>
-          </section>
+      <GridContainer className="md:my-8 flex flex-col-reverse gap-14 items-center md:justify-around md:flex-row md:gap-0 ">
+        <div className="flex justify-center w-3/4">
+          <Image
+            src="/imagem-sobre.jpg"
+            alt="Sobre"
+            width={500}
+            height={100}
+            className="shadow-lg w-full md:w-[300] xl:w-[350] 2xl:w-[500] shadow-green-600 object-fill"
+          />
         </div>
+        <section className="w-full px-6 md:w-3/4">
+          <h1 className="text-4xl 2xl:text-5xl text-center font-bold my-8">Sobre</h1>
+          <p className="md:text-2xl text-justify">
+            Somos uma empresa preocupada em oferecer produtos de alta qualidade,
+            aproveitando os recursos de maneira sustentável em busca de
+            satisfação do cliente.
+          </p>
+        </section>
       </GridContainer>
       <Footer />
     </>
